6f814ee59cbae1949dc1ab5e86ce15dce8703ecb
suchyba.bq@gmail.com <suchyba.bq@gmail.com>
2020-04-29 13:42:19 +0200

    Better license notes. Add license file.

--- statki/statek.h
+++ statki/statek.h
@@ -14,6 +14,7 @@
 
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+	If you want to contact me write: suchyba.bq@gmail.com
 */
 #ifndef STATEK_H_INCLUDED
 #define STATEK_H_INCLUDED
